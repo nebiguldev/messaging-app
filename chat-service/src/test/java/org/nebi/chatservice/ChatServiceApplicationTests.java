82b2910d0a65914001a63730cc8e797a99538c43
nebiguldev <gulnebi19@gmail.com>
2024-05-26 21:13:32 +0300

    my firs commit

---
+++ chat-service/src/test/java/org/nebi/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package org.nebi.chatservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ChatServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
